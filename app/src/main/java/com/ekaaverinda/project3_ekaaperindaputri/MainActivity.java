416344938e480aa8ebca47aa09bb083aa70840c1
EkaAperindaPutri <80092497+EkaAperindaPutri@users.noreply.github.com>
2021-08-10 18:27:58 +0700

    Menambahkan program untuk menampilkan listview

--- app/src/main/java/com/ekaaverinda/project3_ekaaperindaputri/MainActivity.java
+++ app/src/main/java/com/ekaaverinda/project3_ekaaperindaputri/MainActivity.java
@@ -2,13 +2,42 @@ package com.ekaaverinda.project3_ekaaperindaputri;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.ColorSpace;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //mendeklarasikan listview var dan menginisialisasi array tipe data string
+    private ListView lvItem;
+    private String[] namanegara = new String[] {
+        "Indonesia","Malaysia","Singapore","Italia",
+        "Inggris", "Belanda", "Argentina", "Chile",
+        "Mesir", "Uganda"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("ListView Sederhana"); //tampil judul
+
+        //Membinding atau memformat data
+        lvItem = (ListView) findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, namanegara);
+
+        //menset data di dalam listview
+        lvItem.setAdapter(adapter);
+
+        lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Memilih : "+namanegara[position], Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
